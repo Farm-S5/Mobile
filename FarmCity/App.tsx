564698ad15d096c294c153618743109cd5c413ba
Rakotoarivo Loick <120171050+Avocadorable0@users.noreply.github.com>
2024-02-05 10:27:55 +0300

    login page

--- FarmCity/App.tsx
+++ FarmCity/App.tsx
@@ -2,6 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './screens/LoginScreen';
+import SignUpSreens from './screens/SignUpSreens';
+import HomeScreen from './screens/HomeScreen';
+import { Header } from 'react-native/Libraries/NewAppScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -9,7 +13,9 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
+        <Stack.Screen options={{headerShown:false}} name="Login" component={LoginScreen} />
+        <Stack.Screen name="Sign up" component={SignUpSreens} />
+        <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- FarmCity/screens/LoginScreen.js
+++ FarmCity/screens/LoginScreen.js
@@ -1,14 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { TextInput,KeyboardAvoidingView, StyleSheet, Text, View } from "react-native";
+import React from "react";
 
 const LoginScreen = () => {
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
-  )
-}
+    <KeyboardAvoidingView styles={styles.container} behavior="padding">
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Nom d'utilisteur"
+          // value={ }
+          // onChangeText={text => }
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Mot de passe"
+          // value={ }
+          // onChangeText={text => }
+          style={styles.input}
+          secureTextEntry
+        />
+      </View>
+    </KeyboardAvoidingView>
+  );
+};
 
-export default LoginScreen
+export default LoginScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent:'center',
+        alignItems:'center',
+    }
+});
