ce9aca53434eaf21c3741373892152e4469e7ecb
Rakotoarivo Loick <120171050+Avocadorable0@users.noreply.github.com>
2024-02-05 12:15:42 +0300

    Last up date on Login and register

--- FarmCity/App.tsx
+++ FarmCity/App.tsx
@@ -3,7 +3,6 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
-import SignUpSreens from './screens/SignUpSreens';
 import HomeScreen from './screens/HomeScreen';
 import { Header } from 'react-native/Libraries/NewAppScreen';
 
@@ -14,7 +13,6 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen options={{headerShown:false}} name="Login" component={LoginScreen} />
-        <Stack.Screen name="Sign up" component={SignUpSreens} />
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
--- FarmCity/screens/LoginScreen.js
+++ FarmCity/screens/LoginScreen.js
@@ -7,11 +7,66 @@ import {
   View,
   Platform,
 } from "react-native";
-
-import React,{useState} from "react";
+import { useNavigation } from "@react-navigation/native";
+import React, { useEffect, useState } from "react";
 const LoginScreen = () => {
-    const [name,setName] = useState('')
-    const [pass,setPass] = useState('')
+  const [name, setName] = useState("");
+  const [pass, setPass] = useState("");
+  const [message, setMessage] = useState(null);
+  const navigation = useNavigation();
+
+  const handleLogin = async () => {
+    try {
+      const response = await fetch(
+        "https://backend-production-b756.up.railway.app/personne/checkPersonne",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            nameUser: name,
+            passUser: pass,
+          }),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Login failed");
+      }
+      const data = await response.json();
+      if (data) {
+        navigation.navigate("Home", { userId: data});
+      } else {
+        setMessage("Utilisateur non trouvé");
+      }
+    } catch (error) {
+      console.error("Error during login:", error);
+    }
+  };
+
+  const handleRegister = async () => {
+    try {
+      const response = await fetch(
+        "https://backend-production-b756.up.railway.app/personne/insertPersonne",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            nameUser: name,
+            passUser: pass,
+          }),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Registration failed");
+      }
+      setMessage("Inscription réussie");
+    } catch (error) {
+      console.error("Error during registration:", error);
+    }
+  };
   return (
     <KeyboardAvoidingView
       style={styles.container}
@@ -21,28 +76,33 @@ const LoginScreen = () => {
         <TextInput
           placeholder="Nom d'utilisateur"
           value={name}
-          onChangeText={text => setName(text)}
+          onChangeText={(text) => setName(text)}
           style={styles.input}
         />
         <TextInput
           placeholder="Mot de passe"
           value={pass}
-          onChangeText={text => setPass(text)}
+          onChangeText={(text) => setPass(text)}
           style={styles.input}
           secureTextEntry
         />
       </View>
       <View style={styles.btnContainer}>
-        <TouchableOpacity onPress={() => {}} style={styles.btn}>
+        <TouchableOpacity onPress={handleLogin} style={styles.btn}>
           <Text style={styles.btnText}>Se connecter</Text>
         </TouchableOpacity>
         <TouchableOpacity
-          onPress={() => {}}
+          onPress={handleRegister}
           style={[styles.btn, styles.btnOutLine]}
         >
           <Text style={styles.btnOutLineText}>S'inscrire</Text>
         </TouchableOpacity>
       </View>
+      {message && (
+        <View style={styles.messageContainer}>
+          <Text style={styles.messageText}>{message}</Text>
+        </View>
+      )}
     </KeyboardAvoidingView>
   );
 };
@@ -56,42 +116,53 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   inputContainer: {
-    width: '80%',
+    width: "80%",
   },
   input: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     paddingHorizontal: 15,
     paddingVertical: 10,
     borderRadius: 10,
     marginTop: 10,
   },
   btnContainer: {
-    width: '60%',
-    justifyContent: 'center',
-    alignItems:'center',
-    marginTop:10,
+    width: "60%",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 10,
   },
   btn: {
-    backgroundColor: '#0782F9',
-    width: '100%',
+    backgroundColor: "#0782F9",
+    width: "100%",
     padding: 15,
-    borderRadius:10,
-    alignItems: 'center',
+    borderRadius: 10,
+    alignItems: "center",
   },
   btnOutLine: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     marginTop: 5,
-    borderColor: '#0782F9',
+    borderColor: "#0782F9",
     borderWidth: 2,
   },
   btnText: {
-    color:'white',
-    fontWeight: '700',
+    color: "white",
+    fontWeight: "700",
     fontSize: 16,
   },
   btnOutLineText: {
-    color:'#0782F9',
-    fontWeight: '700',
+    color: "#0782F9",
+    fontWeight: "700",
     fontSize: 16,
+  },
+  messageContainer: {
+    marginTop: 20,
+    backgroundColor: "#4CAF50",
+    padding: 10,
+    borderRadius: 5,
+  },
+  messageText: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
