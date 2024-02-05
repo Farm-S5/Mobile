e22eb28e0b4e94de44c628fa753b7e55a32163a5
Rakotoarivo Loick <120171050+Avocadorable0@users.noreply.github.com>
2024-02-05 11:17:20 +0300

    login page

--- FarmCity/screens/LoginScreen.js
+++ FarmCity/screens/LoginScreen.js
@@ -1,23 +1,47 @@
-import { TextInput,KeyboardAvoidingView, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  TouchableOpacity,
+  TextInput,
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+} from "react-native";
 
+import React,{useState} from "react";
 const LoginScreen = () => {
+    const [name,setName] = useState('')
+    const [pass,setPass] = useState('')
   return (
-    <KeyboardAvoidingView styles={styles.container} behavior="padding">
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
       <View style={styles.inputContainer}>
         <TextInput
-          placeholder="Nom d'utilisteur"
-          // value={ }
-          // onChangeText={text => }
+          placeholder="Nom d'utilisateur"
+          value={name}
+          onChangeText={text => setName(text)}
           style={styles.input}
         />
         <TextInput
           placeholder="Mot de passe"
-          // value={ }
-          // onChangeText={text => }
+          value={pass}
+          onChangeText={text => setPass(text)}
           style={styles.input}
           secureTextEntry
         />
+      </View>
+      <View style={styles.btnContainer}>
+        <TouchableOpacity onPress={() => {}} style={styles.btn}>
+          <Text style={styles.btnText}>Se connecter</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {}}
+          style={[styles.btn, styles.btnOutLine]}
+        >
+          <Text style={styles.btnOutLineText}>S'inscrire</Text>
+        </TouchableOpacity>
       </View>
     </KeyboardAvoidingView>
   );
@@ -26,9 +50,48 @@ const LoginScreen = () => {
 export default LoginScreen;
 
 const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        justifyContent:'center',
-        alignItems:'center',
-    }
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  inputContainer: {
+    width: '80%',
+  },
+  input: {
+    backgroundColor: 'white',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 10,
+  },
+  btnContainer: {
+    width: '60%',
+    justifyContent: 'center',
+    alignItems:'center',
+    marginTop:10,
+  },
+  btn: {
+    backgroundColor: '#0782F9',
+    width: '100%',
+    padding: 15,
+    borderRadius:10,
+    alignItems: 'center',
+  },
+  btnOutLine: {
+    backgroundColor: 'white',
+    marginTop: 5,
+    borderColor: '#0782F9',
+    borderWidth: 2,
+  },
+  btnText: {
+    color:'white',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+  btnOutLineText: {
+    color:'#0782F9',
+    fontWeight: '700',
+    fontSize: 16,
+  },
 });
