1d15795e25988c61252ab34bf7c5051618594147
Rakotoarivo Loick <120171050+Avocadorable0@users.noreply.github.com>
2024-02-05 10:09:33 +0300

    lest get started

---
+++ FarmCity/screens/SignUpSreens.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SignUpSreens = () => {
+  return (
+    <View>
+      <Text>SignUpSreens</Text>
+    </View>
+  )
+}
+
+export default SignUpSreens
+
+const styles = StyleSheet.create({})
